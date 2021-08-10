ab65ac23f9c9608166d5af168f68fef86c6e82ef
Achmad An'im <animfahmy@yahoo.co.id>
2021-08-10 22:21:41 +0700

    tidak lagi menggunakan sqlite

--- application/models/Daftar_model.php
+++ application/models/Daftar_model.php
@@ -18,12 +18,7 @@ class Daftar_model extends CI_Model{
 			'nama_sekolah'	=> $data['ns'],
 			'password'		=> md5($data['password'])
 		]);
-		if ($id = $this->db->insert_id()) {
-			$this->dbfast = $this->load->database('fast', TRUE);
-			$this->dbfast->insert('sekolah', [
-				'id_sekolah' 	=> $id,
-				'kode_sekolah'	=> $data['ks']
-			]);
+		if ($this->db->insert_id()) {
 			$this->session->set_flashdata('nama_sekolah', $data['ns']);
 			redirect('daftar/sukses');
 		}
@@ -32,10 +27,10 @@ class Daftar_model extends CI_Model{
 
 	private function is_kode_sekolah_exist($ks)
 	{
-		$this->dbfast = $this->load->database('fast', TRUE);
-		$this->dbfast->select('id_sekolah');
-		$this->dbfast->where('kode_sekolah', $ks);
-		$row = $this->dbfast->get('sekolah')->row();
+		$this->dbselect = $this->load->database('select', TRUE);
+		$this->dbselect->select('id_sekolah');
+		$this->dbselect->where('kode_sekolah', $ks);
+		$row = $this->dbselect->get('sekolah')->row();
 		if (isset($row->id_sekolah)) {
 			$this->session->set_flashdata('error', 'Kode Sekolah sudah digunakan, silahkan coba kode sekolah yang lain.');
 			redirect('daftar');
